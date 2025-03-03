f69ddd72aebaaa0f5586ac29ac0a955178a39003
li-cris <cli.2@alumni.unav.es>
2025-03-03 13:51:53 +0100

    trabajo temp

--- scripting_temp_cris.R
+++ scripting_temp_cris.R
@@ -1,40 +1,79 @@
-# Script ----
+library(caTools)
+# Cosas que he añadido: multivariante con HER2, sin HER2, tabla de tipos de cáncer + frec
+# He cambiado el sourcing de las funciones
+
+# Funciona estando en una carpeta igual a la del repositorio
+source('./contar_NAs.R')
+source('./escalar.R')
+source('./filtrar_datos.R')
+# source('./bivariante.R')
+source("./src/fun_resumen_dataset.R")
+source("./src/fun_dummy.R")
+source("./src/fun_normalizacion.R")
+source("./src/fun_univar_pvalue.R")
+source("./src/fun_rerun_multivariant.R")
+source("./src/fun_accuracy.R")
+
+
+# Tranformaciones y funciones
 datos <- read_csv("pacientes_cancer3.csv")
 filtrados <- filtrar_datos(datos)
+datos_finales <- nuevo_dataset_dummy(filtrados)
 
-datos_HER2 <- filtrados |> select(-CRPlevel) |> remove_na()
-
-# Dummies y escalado
-datos_HER2dummy <- nuevo_dataset_dummy(datos_HER2)
-datos_HER2escalado <- nuevo_dataset_normalizado(datos_HER2dummy)
-
-# Bivariante HER2
-datos_HER2 <- datos_HER2escalado
-tabla_pvalores_univar <- table_univar_sig(datos_HER2 |> select(-Remision), datos_HER2$Remision)
-tabla_pvalores_univar[tabla_pvalores_univar['P.Valores']<0.05, ]
+# P-valores bivariante
+tabla_pvalores_univar <- table_univar_sig(datos_finales |> select(-Remision), datos_finales$Remision)
+tabla_significativos <- tabla_pvalores_univar[tabla_pvalores_univar['P.Valores']<0.05, ]
 colnames_signif <- tabla_pvalores_univar['Nombres'][tabla_pvalores_univar['P.Valores']<0.05]
 
-# Training 
-library(caTools)
-set.seed(123)
-size_training <- 0.8
+# Datos con col de HER2 y modelo multivariante ----
+## Quitar col y NA
+datos_HER2 <- datos_finales |> select(-CRPlevel) |> remove_na()
+## Modelo logistico
+m.logistico <- glm(datos_HER2$Remision~., data = datos_HER2[colnames_signif], family = "binomial")
+P.valores_multi <- summary(m.logistico)$coefficients[, 4]
+P.valores_multi <- find_sig_variables(datos_HER2 |> select(-Remision), datos_HER2$Remision, umbral = 0.75, P.valores_multi)
+sig_variables_HER2 <- names(P.valores_multi[-1])
+## Tabla con Nombres y P-valor significativos (con HER2)
+tabla_significativos_multi <- as.data.frame(P.valores_multi[-1]) |> reframe(P.valor = P.valores_multi[-1]) |> mutate(Nombres = names(P.valores_multi[-1])) |> select(Nombres, P.valor)
 
-# splitting data
-split <- sample.split(datos_HER2$Remision, SplitRatio = size_training)
+# Datos SIN col de HER2, con más pacientes y modelo multivariante ----
+## Quitar col y NA
+datos_pac <- datos_finales |> select(-c(CRPlevel, Mut_HER2)) |> remove_na() 
+## Columnas significativas sin HER2
+colnames_signif_pac <- colnames_signif[!colnames_signif%in%"Mut_HER2"]
+## Modelo logístico
+m.logistico_pac <- glm(datos_pac$Remision~., data = datos_pac[colnames_signif_pac], family = "binomial")
+P.valores_multi_pac <- summary(m.logistico_pac)$coefficients[, 4]
+P.valores_multi_pac <- find_sig_variables(datos_pac |> select(-Remision), datos_pac$Remision, umbral = 0.75, P.valores_multi_pac)
+sig_variables_pac <- names(P.valores_multi_pac[-1])
+## Tabla con Nombres y P-valor significativos (sin HER2)
+tabla_significativos_multi_pac <- as.data.frame(P.valores_multi_pac[-1]) |> reframe(P.valor = P.valores_multi_pac[-1]) |> mutate(Nombres = names(P.valores_multi_pac[-1])) |> select(Nombres, P.valor)
 
-# creating training dataset
-trainingSet <- subset(datos_HER2, split == TRUE)
 
-# creating test data set
-testSet <- subset(datos_HER2, split == FALSE)
+# Model training early ----
+train_and_test_first <- function(dataset, col_dataset_dep, size_training, sig_variable_names, threshold){
+  name_dep <- colnames(col_dataset_dep)
+  set.seed(123)
+  size_training <- size_training
+  
+  # splitting data
+  split <- sample.split(dataset[[paste0(name_dep)]], SplitRatio = size_training)
+  
+  # creating training dataset
+  trainingSet <- subset(dataset, split == TRUE)
+  
+  # creating test data set
+  testSet <- subset(dataset, split == FALSE)
+  
+  # model
+  glm_model <- glm(trainingSet[[paste0(name_dep)]]~.,
+                      data = trainingSet[sig_variable_names],
+                      family = "binomial")
+  
+  # prediction
+  glm_model_predict <- predict(glm_model, testSet, type = "response")
+  # accuracy
+  accuracy <- calc_accuracy(glm_model_predict, ifelse(testSet[[paste0(name_dep)]]==1, "Sí", "No"), threshold = threshold)
+}
 
-# model
-cancer_model <- glm(trainingSet$Remision~.,
-                    data = trainingSet[sig_variables_HER2],
-                    family = "binomial")
-
-# prediction
-cancer_model_predict <- predict(cancer_model, testSet, type = "response")
-
-# accuracy
-accuracy <- calc_accuracy(cancer_model_predict, ifelse(testSet$Remision==1, "Sí", "No"), threshold = 0.6)
+train_and_test_first(datos_pac, datos_pac['Remision'], size_training = 0.8, sig_variable_names = sig_variables_pac, threshold = 0.5)
