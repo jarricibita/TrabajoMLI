8eb74e0c61d956c31a259aed907bc2dd510f1e99
Cristina Li <cli.2@alumni.unav.es>
2025-03-03 13:16:27 +0100

    Update setup_presentacion.R

--- setup_presentacion.R
+++ setup_presentacion.R
@@ -1,6 +1,12 @@
 # Script para setup
 
 # Source de funciones y otras preparaciones para ejecutar Presentacion.R
-
-source("./filtrar_datos.R")
-source("./contar_NAs.R")
+source('./contar_NAs.R')
+source('./escalar.R')
+source('./filtrar_datos.R')
+# source('./bivariante.R')
+source("./src/fun_resumen_dataset.R")
+source("./src/fun_dummy.R")
+source("./src/fun_normalizacion.R")
+source("./src/fun_univar_pvalue.R")
+source("./src/fun_rerun_multivariant.R")
