c5eca85f6af26342c8ac6f046c8c04195d604694
li-cris <cli.2@alumni.unav.es>
2025-03-03 12:52:42 +0100

    presentacion con multivariante y funcion de cross validation

--- Presentacion.R
+++ Presentacion.R
@@ -1,16 +1,55 @@
 library(shiny)
 library(readr)
+library(caTools)
+# Cosas que he añadido: multivariante con HER2, sin HER2, tabla de tipos de cáncer + frec
+# He cambiado el sourcing de las funciones
 
+# Funciona estando en una carpeta igual a la del repositorio
 source('./contar_NAs.R')
 source('./escalar.R')
 source('./filtrar_datos.R')
-source('./bivariante.R')
-source('./nuevo_dataset_dummy.R')
+# source('./bivariante.R')
+source("./src/fun_resumen_dataset.R")
+source("./src/fun_dummy.R")
+source("./src/fun_normalizacion.R")
+source("./src/fun_univar_pvalue.R")
 
+# Tranformaciones y funciones
 datos <- read_csv("pacientes_cancer3.csv")
 filtrados <- filtrar_datos(datos)
 datos_finales <- nuevo_dataset_dummy(filtrados)
 
+# P-valores bivariante
+tabla_pvalores_univar <- table_univar_sig(datos_finales |> select(-Remision), datos_finales$Remision)
+tabla_significativos <- tabla_pvalores_univar[tabla_pvalores_univar['P.Valores']<0.05, ]
+colnames_signif <- tabla_pvalores_univar['Nombres'][tabla_pvalores_univar['P.Valores']<0.05]
+
+# Datos con col de HER2 y modelo multivariante
+## Quitar col y NA
+datos_HER2 <- datos_finales |> select(-CRPlevel) |> remove_na()
+## Modelo logistico
+m.logistico <- glm(datos_HER2$Remision~., data = datos_HER2[colnames_signif], family = "binomial")
+P.valores_multi <- summary(m.logistico)$coefficients[, 4]
+## Tabla con Nombres y P-valor significativos (con HER2)
+tabla_significativos_multi <- as.data.frame(P.valores_multi) |> reframe(P.valor = P.valores_multi) |> mutate(Nombres = names(P.valores_multi)) |> select(Nombres, P.valor)
+
+# Datos SIN col de HER2, con más pacientes y modelo multivariante
+## Quitar col y NA
+datos_pac <- datos_finales |> select(-c(CRPlevel, Mut_HER2)) |> remove_na() 
+## Columnas significativas sin HER2
+colnames_signif_pac <- colnames_signif[!colnames_signif%in%"Mut_HER2"]
+## Modelo logístico
+m.logistico_pac <- glm(datos_pac$Remision~., data = datos_pac[colnames_signif_pac], family = "binomial")
+P.valores_multi_pac <- summary(m.logistico_pac)$coefficients[, 4]
+## Tabla con Nombres y P-valor significativos (sin HER2)
+tabla_significativos_multi_pac <- as.data.frame(P.valores_multi_pac) |> reframe(P.valor = P.valores_multi_pac) |> mutate(Nombres = names(P.valores_multi_pac)) |> select(Nombres, P.valor)
+
+
+
+
+#####################################################################################
+
+# SHINY APP
 ui <- fluidPage(
   titlePanel("Trabajo final MLI"),
   
@@ -18,7 +57,7 @@ ui <- fluidPage(
     sidebarPanel(
       h3("Controles Interactivos"),
       selectInput("Modelo", "Elige qué quieres ver:", 
-                  choices = c('Distribución de variables', "Bivariante", "Multivariante", "CV + predicciones", 'Tipos de cáncer')),
+                  choices = c("Distribución de variables", "Bivariante", "Multivariante HER2", "Multivariante sin HER2", "CV + predicciones HER2", "CV + predicciones sin HER2", "Tipos de cáncer")),
       
       conditionalPanel(
         condition = "input.Modelo == 'Distribución de variables'",
@@ -47,13 +86,16 @@ server <- function(input, output, session) {
     if (input$Modelo == 'Distribución de variables') {
       return(contar_NAs(datos))
     } else if (input$Modelo == 'Bivariante') {
-      return(bivariante(datos_finales))
-    } else if (input$Modelo == 'Multivariante') {
-      
+      return(tabla_significativos)
+    } else if (input$Modelo == 'Multivariante HER2') {
+      return(tabla_significativos_multi[-1, ])
+    } else if (input$Modelo == 'Multivariante sin HER2') {
+      return(tabla_significativos_multi_pac[-1, ])
     } else if (input$Modelo == 'CV + predicciones') {
       
     } else if (input$Modelo == 'Tipos de cáncer') {
-      
+      frec_localizacion <- filtrados |> select(-CRPlevel) |>
+        group_by(Localizacion_Primaria) |> summarise(Num = n()) |> mutate(Percentage = Num/(sum(Num))*100)
     }
   })
   
@@ -68,10 +110,11 @@ server <- function(input, output, session) {
         par(mar = c(10, 10, 4, 7))
         barplot(freqs, main = paste('Diagrama de barras de', input$Variable), names.arg = names(freqs), horiz = TRUE, las=2)
       }
-    } else if (input$Modelo == 'CV + predicciones') {
+    } else if (input$Modelo == 'CV + predicciones HER2') {
+      
+    } else if (input$Modelo == 'CV + predicciones sin HER2') { 
       
     } else if (input$Modelo == 'Tipos de cáncer') {
-      
     }
   })
 }
---
+++ src/fun_accuracy.R
@@ -0,0 +1,19 @@
+#'@title Calculo de precisión (accuracy)
+#'
+#'@description Dado predicción y datos de testeo
+#'
+#'@param model_prediction
+#'@param testSet_and_dep
+#'@return accuracy
+#'
+calc_accuracy <- function(model_prediction, testSet_and_dep, threshold){
+  clasificacion <- ifelse(model_prediction > threshold,"Sí","No")
+  clasificacion <- ordered(clasificacion, levels = c("Sí", "No"))
+  testSet_and_dep <- ordered(testSet_and_dep, levels = c("Sí", "No"))
+  
+  mat_res <- table(Predicted = clasificacion, Actual = testSet_and_dep)
+  mat_res
+  accuracy <- (mat_res[1, 1]+mat_res[2, 2])/(sum(mat_res))
+  print(paste0("Precisión del modelo: ", accuracy))
+  return(accuracy)
+}
